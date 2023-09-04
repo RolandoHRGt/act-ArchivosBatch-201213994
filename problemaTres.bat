5e8b0dad80f4f19cd0017e23a9512d1849399c6d
Rolando Hernández <rolandohernandez91r@gmail.com>
2023-09-03 20:55:03 -0600

    Add: problemaTres/resuelto

---
+++ problemaTres.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Arbol de Directorio 
+echo Sistemas Operativos
+
+mkdir "Sistemas Operativos 2023"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software de Sistema"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software de Sistema\Sistemas Operativos"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software de Sistema\Drivers"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software utilitario"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software utilitario\Booteables"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software utilitario\Recuperar Informacion"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software Aplicacion"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software Aplicacion\Procesadores de Texto"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software Aplicacion\Hojas de Calculo"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software Aplicacion\Presentaciones"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software de Programacion"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software de Programacion\Python"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software de Programacion\PHP"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software de Programacion\JAVA"
+mkdir "Sistemas Operativos 2023\Rolando Hernandez\Software de Programacion\C#"
+
+pause
